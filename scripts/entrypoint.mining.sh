92bcec86783a7e284900e39b061155abaab09419
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2018-06-27 16:16:43 -0400

    [ADD] --confirm-external-bind param for daemon [CHG] log level

--- scripts/entrypoint.mining.sh
+++ scripts/entrypoint.mining.sh
@@ -6,9 +6,10 @@ monerod --testnet \
 		--data-dir /monero/blockchain/ \
 		--p2p-bind-ip 127.0.0.1 \
 		--p2p-bind-port 28080 \
-		--log-level 2 \
+		--log-level 1 \
 		--rpc-bind-ip 0.0.0.0 \
 		--rpc-bind-port 28081 \
+		--confirm-external-bind \
 		--detach \
         --start-mining 9wviCeWe2D8XS82k2ovp5EUYLzBt9pYNW2LXUFsZiv8S3Mt21FZ5qQaAroko1enzw3eGr9qC7X1D7Geoo2RrAotYPwq9Gm8 \
         --mining-threads 1
@@ -17,6 +18,7 @@ monero-wallet-rpc --testnet \
 		--rpc-bind-port 28082 \
         --trusted-daemon \
         --rpc-bind-ip 0.0.0.0 \
+        --log-level 1 \
         --confirm-external-bind \
         --disable-rpc-login \
         --wallet-file /monero/wallet/wallet.bin \
